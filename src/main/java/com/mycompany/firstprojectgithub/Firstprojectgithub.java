2a58a0637d1c5d4ff6c3fe2a65b4983919cc22d6
RC_Student_lab <RC_Student_lab@10.0.0.111>
2024-09-09 08:23:22 +0000

    A program to add two numbers

--- src/main/java/com/mycompany/firstprojectgithub/Firstprojectgithub.java
+++ src/main/java/com/mycompany/firstprojectgithub/Firstprojectgithub.java
@@ -12,5 +12,10 @@ public class Firstprojectgithub {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a = 5;
+        int b = 4;
+        int c = a+b;
+        System.out.println("sum : "+c);
+        
     }
 }
